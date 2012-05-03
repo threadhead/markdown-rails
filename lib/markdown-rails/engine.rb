fdc9e5615d3bff14b18d69937bd62e39d0e32bc6
Karl Smith <threadhead@gmail.com>
2012-05-03 10:22:32 -0700

    update some defaults

--- lib/markdown-rails/engine.rb
+++ lib/markdown-rails/engine.rb
@@ -28,7 +28,7 @@ end
 
 MarkdownRails.configure do |config|
   config.render do |markdown_source|
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true, :lax_html_blocks => true)
     markdown.render(markdown_source)
   end
 end
